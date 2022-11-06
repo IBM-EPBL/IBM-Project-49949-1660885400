00d94d3abf1c24104d61bd1b55c71375534873b2
GanapathyPT <18ganapathy04@gmail.com>
2022-11-06 17:59:11 +0530

    upload image functionality

--- Project-Development/Sprint-1/Flask/app.py
+++ Project-Development/Sprint-1/Flask/app.py
@@ -1,7 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from os import path
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
+
+@app.route('/api/predict', methods=["POST"])
+def predict():
+    if request.method != "POST":
+        return render_template('index.html')
+
+    print(request.files)
+    # getting the image and saving it locally
+    image = request.files["file"]
+    root_path = path.dirname(__file__)
+    image_path = path.join(root_path, 'uploads', image.filename)
+    image.save(image_path)
+
+    return {"url": image_path}
--- Project-Development/Sprint-1/Flask/static/js/script.js
+++ Project-Development/Sprint-1/Flask/static/js/script.js
@@ -1,4 +1,7 @@
 const fileDropArea = document.querySelector(`[data-item="file-drop-area"]`);
+const uploadedImage = document.querySelector(`[data-item="uploaded-image"]`);
+const uploadInput = document.querySelector(`[data-item="upload-input"]`);
+const changeImageBtn = document.querySelector(`[data-item="change-image"]`);
 const classifyBtn = document.querySelector(`[data-item="classify-btn"]`);
 
 const highlight = (event) => {
@@ -17,11 +20,50 @@ fileDropArea.addEventListener("dragenter", highlight, false);
 fileDropArea.addEventListener("dragover", highlight, false);
 
 fileDropArea.addEventListener("dragleave", unhighlight, false);
+fileDropArea.addEventListener("drop", unhighlight, false);
 fileDropArea.addEventListener(
   "drop",
-  (event) => {
-    unhighlight(event);
-    const file = event.dataTransfer.files[0];
+  async (event) => {
+    const dataTransfer = event.dataTransfer;
+    uploadInput.files = dataTransfer.files;
   },
   false
 );
+
+uploadInput.addEventListener("change", (event) => {
+  const file = uploadInput.files[0];
+  if (file) {
+    const url = URL.createObjectURL(file);
+    uploadedImage.src = url;
+
+    uploadedImage.classList.remove("hidden");
+    fileDropArea.classList.add("hidden");
+    changeImageBtn.classList.remove("hidden");
+    classifyBtn.classList.remove("hidden");
+  }
+});
+
+changeImageBtn.addEventListener("click", (event) => {
+  uploadInput.click();
+});
+
+classifyBtn.addEventListener("click", (event) => {
+  const file = uploadInput.files[0];
+  predict(file);
+});
+
+async function predict(file) {
+  if (!file) return;
+
+  const data = new FormData();
+  data.append("file", file);
+
+  try {
+    await fetch("/api/predict", {
+      method: "POST",
+      body: data,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
